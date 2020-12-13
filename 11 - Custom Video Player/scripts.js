79333ea696cc0e5091f8b8d638f351f2c40c4607
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-13 17:36:46 +0900

    added scrubbing + made fullscreen btn working

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,6 +5,9 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+const fullScreenBtn = player.querySelector("[data-fullScreen]");
+
+let mousedown = false;
 
 const togglePlayer = () => {
   if (video.paused) {
@@ -38,8 +41,6 @@ const handleProgressUpdate = (event) => {
 
 const scrub = (event) => {
   const percent = event.offsetX / video.clientWidth;
-  console.log(percent);
-  console.log(video.duration);
   video.currentTime = video.duration * percent;
 };
 
@@ -47,9 +48,17 @@ video.addEventListener("click", togglePlayer);
 video.addEventListener("play", updateBtn);
 video.addEventListener("pause", updateBtn);
 video.addEventListener("timeupdate", handleProgressUpdate);
+
 progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
+
 toggle.addEventListener("click", togglePlayer);
+
 skipButtons.forEach((btn) => btn.addEventListener("click", skip));
+fullScreenBtn.addEventListener("click", () => video.requestFullscreen());
+
 ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
